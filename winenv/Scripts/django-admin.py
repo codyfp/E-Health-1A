3a1a6497fb6345f0f6f77bc02b4b98fca086d58c
codyfp <fisherpeel.c@gmail.com>
2020-04-19 15:44:29 +1000

    windows virtual env setup

---
+++ winenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\fishe\Desktop\Django Projects\SES Test\SES-1A-4PM---Group-4\winenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
