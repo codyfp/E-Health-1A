f30723b4575197d6990bf013d0a08ef4e26e03a0
Aryan Gupta <aryan.gupta@student.uts.edu.au>
2020-04-19 14:53:59 +1000

    Created views and urls

--- HealthOnline/settings.py
+++ HealthOnline/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'OnlineConsultation',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
