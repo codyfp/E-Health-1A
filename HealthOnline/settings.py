04d09de66567abf6e6158cd839e5785e30b5c5e7
calvinlay <13254471@student.uts.edu.au>
2020-04-21 15:13:28 +1000

    Changed code to make it compatibile with local directories - also added /home, pages, templates and static folder

--- HealthOnline/settings.py
+++ HealthOnline/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'OnlineConsultation',
+    'pages',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'HealthOnline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
--- HealthOnline/urls.py
+++ HealthOnline/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pages import views
 
 urlpatterns = [
         path('admin/', admin.site.urls),
-        path('', include('OnlineConsultation.urls'))
+        path('', include('OnlineConsultation.urls')),
+    path('home/', views.home_view, name='home'),
 
 ]
