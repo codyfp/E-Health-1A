6b8ecd2f5f73462ff6b7bcf0c4f1368023d70511
Aryan Gupta <aryan.gupta@student.uts.edu.au>
2020-04-24 16:13:36 +1000

    Deleting files from master

--- winenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\fishe\Desktop\Django Projects\SES Test\SES-1A-4PM---Group-4\winenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
