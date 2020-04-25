e9cdc5b4041c895af8506eae2d7eda2bba887417
Mehmet Ata Bal <13396700@student.uts.edu.au>
2020-04-26 00:12:25 +1000

    Cleaned redundant code

--- HealthOnline/System/models.py
+++ HealthOnline/System/models.py
@@ -49,24 +49,6 @@ class Certificates(models.Model):
     certificate = models.FileField(upload_to=('user_'+str(id)+'/Certificate/'))
     doctor      = models.ForeignKey(UserProfile, blank=False, null=False, on_delete=models.PROTECT)    
 
-'''
-class Patient(models.Model):
-    user                = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, default=0) 
-    gender              = models.ForeignKey(Gender, blank=True, null=True, on_delete=models.PROTECT)
-    birth_date          = models.DateField(blank=True, null=True)
-    street              = models.CharField(max_length=150, null=True, blank=True)
-    suburb              = models.CharField(max_length=50, null=True, blank=True)
-    state               = models.ForeignKey(State, blank=True, null=True, on_delete=models.PROTECT)
-    postal_code         = models.CharField(max_length=20, null=True, blank=True)
-    country             = models.ForeignKey(Country, blank=True, null=True, on_delete=models.PROTECT)
-    mobile_phone        = models.CharField(max_length=50, null=True, blank=True)
-    created_date        = models.DateTimeField(null=True, blank=True)
-    last_update         = models.DateTimeField(null=True, blank=True)
-    Identity_Document   = models.FileField(upload_to=('user_'+str(id)+'/Identity/'))
-    is_doctor           = models.BooleanField(default=False)
-    is_patient          = models.BooleanField(default=True)
-'''
-
 class Prescriptions(models.Model):
     doctor  = models.ForeignKey(User, related_name='doctor', blank=False, null=False, on_delete=models.PROTECT)
     patient = models.ForeignKey(User, related_name='patient', blank=False, null=False, on_delete=models.PROTECT)
