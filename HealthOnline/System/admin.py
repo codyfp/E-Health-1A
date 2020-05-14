d0540e60d7f0fa559fa27718943eba8fc54ea044
A-Ishak <asad.ishak@outlook.com>
2020-05-14 18:14:12 +1000

    Logout

--- HealthOnline/System/admin.py
+++ HealthOnline/System/admin.py
@@ -5,3 +5,12 @@ from System.models import Prescription, Consultation
 
 admin.site.register(Prescription)
 admin.site.register(Consultation)
+
+from django.contrib import admin
+from django_google_maps import widgets as map_widgets
+from django_google_maps import fields as map_fields
+
+class MapAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        map_fields.AddressField: {'widget': map_widgets.GoogleMapsAddressWidget},
+    }
